53d14046fdd231744db6d0b40a0c8e32883c23c2
andresitungkirr <andresitungkir4857@gmail.com>
2026-04-08 13:51:32 +0700

    Andre
    Sukses

--- t02_03.c
+++ t02_03.c
@@ -4,23 +4,27 @@
 
 int main() {
     char operator_pilihan;
-
-    scanf("%c", &operator_pilihan);
-
+    
+    // Menerima input operator dan langsung mencetaknya ulang
+    scanf(" %c", &operator_pilihan);
+    printf("%c\n", operator_pilihan);
+    
     int hasil = 0;
     if (operator_pilihan == '*') {
-        hasil = 1;
+        hasil = 1; 
     }
     
     for (int i = 0; i < 4; i++) {
         int angka_masukan;
+        // Menerima angka dan langsung mencetaknya ulang
         scanf("%d", &angka_masukan);
-
+        printf("%d\n", angka_masukan);
+        
         if (angka_masukan == -1) {
             printf("0\n");
             break;
         }
-
+        
         if (operator_pilihan == '+') {
             hasil = hasil + angka_masukan;
         } else if (operator_pilihan == '-') {
@@ -28,7 +32,8 @@ int main() {
         } else if (operator_pilihan == '*') {
             hasil = hasil * angka_masukan;
         }
-
+        
+        // Menampilkan hasil
         printf("%d\n", hasil);
     }
     
